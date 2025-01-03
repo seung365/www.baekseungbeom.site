41b3b40052fd0786bc049f3d0c2acd893d75c4a8
beom <bdh3659@naver.com>
2025-01-04 02:43:27 +0900

    feat: vercel analytics 추가

--- src/main.tsx
+++ src/main.tsx
@@ -1,5 +1,6 @@
 // main.tsx
 import { Global } from "@emotion/react"
+import { Analytics } from "@vercel/analytics/react"
 import { StrictMode } from "react"
 import { createRoot } from "react-dom/client"
 import App from "./App.tsx"
@@ -9,5 +10,6 @@ createRoot(document.getElementById("root")!).render(
   <StrictMode>
     <Global styles={GlobalStyles} />
     <App />
+    <Analytics />
   </StrictMode>,
 )
