26be0689e2ed6d2d031d8ce73e6433fce411c938
beom <bdh3659@naver.com>
2025-01-04 02:15:46 +0900

    feat: IntersectionObserver hook 추가

---
+++ src/hooks/useIntersectionObserver.ts
@@ -0,0 +1,22 @@
+import { useEffect, useRef, useState } from "react"
+
+const useIntersectionObserver = (options = { threshold: 0.1 }) => {
+  const [isVisible, setIsVisible] = useState(false)
+  const sectionRef = useRef<HTMLDivElement>(null)
+
+  useEffect(() => {
+    const observer = new IntersectionObserver(([entry]) => {
+      setIsVisible(entry.isIntersecting)
+    }, options)
+
+    if (sectionRef.current) {
+      observer.observe(sectionRef.current)
+    }
+
+    return () => observer.disconnect()
+  }, [options])
+
+  return { sectionRef, isVisible }
+}
+
+export default useIntersectionObserver
