fed3a86b5487330b022e74d014850310fab9379f
beom <bdh3659@naver.com>
2025-01-04 02:14:09 +0900

    feat: emotion theme type 추가

---
+++ src/emotion.d.ts
@@ -0,0 +1,11 @@
+import "@emotion/react"
+
+declare module "@emotion/react" {
+  export interface Theme {
+    background: string
+    text: string
+    primary: string
+    secondary: string
+    surface: string
+  }
+}
