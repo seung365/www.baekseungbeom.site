5b6d203176771a23a148ce7297c76977d6a20842
beom <bdh3659@naver.com>
2025-01-15 23:47:07 +0900

    chore: projectData 추가

---
+++ src/constants/projectData.ts
@@ -0,0 +1,80 @@
+const projectsData = [
+  {
+    title: "LG webOS 내 스탠바이 앱",
+    period: "2024.09 ~ 2024.12",
+    githubUrl: "https://github.com/KNU-LG",
+    team: "4명 (프론트엔드 2, 백엔드 2)",
+    purpose: "산학 프로젝트로 LG webOS 내 스탠바이 앱을 제작해 오픈소스화를 목적으로한 프로젝트",
+    mainTasks: [
+      "기획서 기반 개발 문서 제작 및 기능 설계, 디자인 참여",
+      "스탠바이 앱 제작 및 오픈소스 등록(예정)",
+      "테크 리더 및 프론트엔드 리더로 git organization 관리",
+      "프론트엔드 구현 (기여도 80%이상)",
+    ],
+    tech: "React · TypeScript · Emotion · TanStack Query · React Hook Form · React-Use-Gesture · ESLint · Prettier · webOS",
+  },
+  {
+    title: "1.618 (미술 중개 웹사이트)",
+    period: "2024.08 ~ 2024.11",
+    githubUrl: "https://github.com/kakao-tech-campus-2nd-step3/Team9_FE",
+    team: "7명 (프론트엔드 2, 백엔드 5)",
+    purpose: "신진 작가, 미대생 발굴 및 전시 작품 중개 웹사이트",
+    mainTasks: [
+      "아이디어부터 기획 및 디자인 과정 모두 참여",
+      "프론트엔드 테크 리더로 멘토님과 일정 조율",
+      "프론트엔드 개발 (기여도 50%)",
+    ],
+    tech: "React · TypeScript · Emotion · TanStack Query · React Hook Form · Chakra UI · Zustand · Pnpm · Vite · ESLint · Prettier · Amazon S3 · GitHub Actions",
+  },
+  {
+    title: "알파메일 (메일 앱 생성 웹사이트)",
+    period: "2024.07 ~ 2024.08",
+    githubUrl: "https://github.com/Beside-Potenday",
+    team: "6명 (PM 1, 디자이너 1, 프론트엔드 2, 백엔드 2)",
+    purpose:
+      "대학생 및 직장인에게 편리한 툴을 제공하고자 하는 아이디어에서 시작해 CLOVA X를 도입해 자동으로 메일을 생성하고 관리할 수 있는 웹사이트입니다.",
+    mainTasks: [
+      "아이디어부터 기획 참여",
+      "프론트엔드 리더로 개발 진행 (PM과 백엔드와의 협의를 대표로 진행, 기여도 80%)",
+    ],
+    tech: "React · TypeScript · Emotion · TanStack Query · React Hook Form · ESLint · Prettier · MacOS",
+  },
+  {
+    title: "뇌졸중 자가진단 웹",
+    period: "2024.05 ~ 2024.06",
+    githubUrl: "https://github.com/seung365/Stroke-self-diagnosis",
+    team: "4명 (프론트엔드 1, 백엔드 2, 머신러닝 1)",
+    purpose: "데이터를 바탕으로 뇌졸중 확률을 보여주고 근처 병원을 소개해주는 웹사이트입니다.",
+    mainTasks: ["아이디어부터 기획, 개발까지 참여", "소프트웨어 설계 명세서 작성", "프론트엔드 개발 진행(기여도 100%)"],
+    tech: "HTML · CSS · JavaScript",
+  },
+  {
+    title: "다배워",
+    period: "2023.08",
+    githubUrl: "https://github.com/seung365/android_Koreahackerton",
+    team: "5명 (프론트엔드 3, 백엔드 2)",
+    purpose:
+      "평생 교육 추천 웹사이트에서 더 나아가 앱서비스를 통해 노인들에게 다양한 교육을 추천해주고 연결해주는 어플입니다.",
+    mainTasks: [
+      "아이디어부터 기획, 개발까지 참여",
+      "피그마를 통한 와이어프레임, UI 제작",
+      "프론트엔드 개발 진행(기여도 40%)",
+    ],
+    tech: "Java · Android Studio · Firebase",
+  },
+  {
+    title: "평생 교육 추천 웹",
+    period: "2023.06",
+    githubUrl: "https://github.com/seung365/cppva",
+    team: "4명 (프론트엔드 2, 백엔드 2)",
+    purpose: "고령화 시대에 노인들에게도 다양한 교육을 받을 수 있도록 추천해주는 웹사이트입니다.",
+    mainTasks: [
+      "아이디어부터 기획, 개발까지 참여",
+      "프론트엔드 개발 진행(기여도 30%)",
+      "애저를 통한 배포 및 github action을 활용하여 CI/CD 연결",
+    ],
+    tech: "HTML · CSS · JavaScript · Azure · GitHub Actions",
+  },
+];
+
+export default projectsData;
