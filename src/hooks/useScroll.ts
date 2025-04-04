8f9b450e9c581b28b072ffa3dfe9771e70df6fb6
beom <bdh3659@naver.com>
2025-04-04 23:48:23 +0900

    chore: useScroll 내 Ref들 타입 수정

--- src/hooks/useScroll.ts
+++ src/hooks/useScroll.ts
@@ -1,28 +1,28 @@
-import { RefObject, useEffect } from "react"
+import { RefObject, useEffect } from "react";
 
 export const useScroll = (
-  sectionRef: RefObject<HTMLElement>,
-  contentRef: RefObject<HTMLDivElement>,
-  contactRef: RefObject<HTMLDivElement>,
+  sectionRef: RefObject<HTMLElement | null>,
+  contentRef: RefObject<HTMLElement | null>,
+  contactRef: RefObject<HTMLDivElement | null>,
 ) => {
   useEffect(() => {
     const handleScroll = () => {
       if (contentRef.current) {
-        contentRef.current.style.setProperty("--scroll-position", window.scrollY.toString())
+        contentRef.current.style.setProperty("--scroll-position", window.scrollY.toString());
       }
       if (contactRef.current) {
-        contactRef.current.style.setProperty("--scroll-position", window.scrollY.toString())
+        contactRef.current.style.setProperty("--scroll-position", window.scrollY.toString());
       }
-    }
+    };
 
-    window.addEventListener("scroll", handleScroll)
-    return () => window.removeEventListener("scroll", handleScroll)
-  }, [contentRef, contactRef])
+    window.addEventListener("scroll", handleScroll);
+    return () => window.removeEventListener("scroll", handleScroll);
+  }, [contentRef, contactRef]);
 
   const handleScrollDown = () => {
-    const nextSection = sectionRef.current?.nextElementSibling
-    nextSection?.scrollIntoView({ behavior: "smooth" })
-  }
+    const nextSection = sectionRef.current?.nextElementSibling;
+    nextSection?.scrollIntoView({ behavior: "smooth" });
+  };
 
-  return { handleScrollDown }
-}
+  return { handleScrollDown };
+};
