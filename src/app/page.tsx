948917a4ec1ca7c2e4a26d7a6d5cdfabbde5a786
beom <bdh3659@naver.com>
2025-04-07 00:51:23 +0900

    refactor: App page.tsx 정리

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,20 +1,11 @@
 "use client";
 
-import { ThemeProvider } from "@emotion/react";
-import { useState } from "react";
-import { darkTheme, lightTheme } from "../styles/theme";
-import { Global } from "@emotion/react";
-import { GlobalStyles } from "../styles/global";
-import { Analytics } from "@vercel/analytics/react";
 import Home from "../components/Home/index";
 
 export default function Page() {
-  const [isDark, setIsDark] = useState(true);
   return (
-    <ThemeProvider theme={isDark ? darkTheme : lightTheme}>
-      <Global styles={GlobalStyles} />
-      <Home isDark={isDark} setIsDark={setIsDark} />
-      <Analytics />
-    </ThemeProvider>
+    <>
+      <Home />
+    </>
   );
 }
