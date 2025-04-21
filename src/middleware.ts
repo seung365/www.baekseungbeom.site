7d5bc04176407824b0a1ec444f726d50960f9fd6
beom <bdh3659@naver.com>
2025-04-21 22:46:10 +0900

    feat: middleware 구현

---
+++ src/middleware.ts
@@ -0,0 +1,20 @@
+import { NextResponse } from "next/server";
+import { NextRequest } from "next/server";
+
+export function middleware(request: NextRequest) {
+  const { pathname } = request.nextUrl;
+
+  if (pathname.startsWith("/admin") && pathname !== "/admin/login") {
+    const token = request.cookies.get("auth")?.value;
+
+    if (!token) {
+      return NextResponse.redirect(new URL("/admin/login", request.url));
+    }
+  }
+
+  return NextResponse.next();
+}
+
+export const config = {
+  matcher: ["/admin", "/admin/:path*"],
+};
