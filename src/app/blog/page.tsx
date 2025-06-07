c9c036b631fe1f3f701df01383b9881b97fed30a
beom <bdh3659@naver.com>
2025-06-08 01:07:59 +0900

    feat: BlogPage 최종 구현

---
+++ src/app/blog/page.tsx
@@ -0,0 +1,16 @@
+import { getAllPosts, getAllTags } from "@/_utils/blogUtils";
+import BlogPageClient from "@/components/Blog/BlogPageClient";
+
+interface SearchParams {
+  tag?: string;
+}
+
+export default function BlogPage({ searchParams }: { searchParams: SearchParams }) {
+  const allPosts = getAllPosts();
+  const allTags = getAllTags();
+  const selectedTag = searchParams.tag;
+
+  const filteredPosts = selectedTag ? allPosts.filter((post) => post.tags.includes(selectedTag)) : allPosts;
+
+  return <BlogPageClient posts={filteredPosts} tags={allTags} selectedTag={selectedTag} />;
+}
