9934950c350db171e9e4844f79fa62c271c92fde
beom <bdh3659@naver.com>
2025-06-08 01:11:08 +0900

    chore: next.config.js에 MDX 지원 추가

--- next.config.js
+++ next.config.js
@@ -1,7 +1,12 @@
+import createMDX from "@next/mdx";
+
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  //  output: 'export',
-  //  distDir: './dist',
+  pageExtensions: ["js", "jsx", "mdx", "ts", "tsx"],
 };
 
-export default nextConfig;
+const withMDX = createMDX({
+  // MDX 플러그인 옵션
+});
+
+export default withMDX(nextConfig);
